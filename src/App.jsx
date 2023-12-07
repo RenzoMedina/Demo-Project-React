99874b22ff3cc175285c9093aa267c1bf6419778
RenzoMedina <medinast30@gmail.com>
2023-12-07 20:56:34 -0300

    Se ha creado una petiticón con un api publica

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [criptos, setCriptos] = useState()
+
+  useEffect(()=>{
+    axios.get(`${import.meta.env.VITE_API_URL}assets`)
+    .then((data)=>{
+      console.log(data)
+      setCriptos(data.data.data)
+    })
+    .catch(()=>{
+      console.log("Error en la petición")
+    })
+  },[])
+
+  if(!criptos) return <><span>Cargando.....</span></>
 
   return (
     <>
       <h1>Hola mundo</h1>
+      <ol>
+      {
+        criptos.map(({id,name, priceUsd}) =>(
+          <li key={id}>Nombre: {name}, Precio: {priceUsd}</li>
+        ))
+      }
+      </ol>
     </>
   )
 }
